12938945ae0c703889d902e5faca94c7ae3cec0e
Jordane <takoukam.jordane@gmail.com>
2024-02-05 09:21:27 +0100

    commit lundi 05-02-2024 90h21

--- lib/models/company.dart
+++ lib/models/company.dart
@@ -1,54 +1,68 @@
 import 'package:isar/isar.dart';
-part 'company.g.dart';
 
 @Collection()
 class Company {
-  Id id = Isar.autoIncrement;
+  @Id()
+  late int id;
 
-  final String name;
-  final String phoneNumber;
-  final String email;
-  final String idApi;
   final String companyName;
   final DateTime registrationDate;
-  final String location;
-  final String createBy;
+  final String ceoName;
+  final String ceoPhoneNumber;
+  final String ceoEmail;
+  final String locationCountry;
+  final String locationCity;
+  final String locationDistrict;
+  final String locationOtherDetails;
+  final String businessType;
+  final String createdBy;
   final bool suspended;
+  final String suspensionReason;
+  final String suspensionDuration;
+  final DateTime? suspensionStart;
+  final DateTime? suspensionEnd;
 
   Company({
-    required this.idApi,
-    required this.name,
-    required this.phoneNumber,
-    required this.email,
     required this.companyName,
     required this.registrationDate,
-    required this.location,
-    required this.createBy,
+    required this.ceoName,
+    required this.ceoPhoneNumber,
+    required this.ceoEmail,
+    required this.locationCountry,
+    required this.locationCity,
+    required this.locationDistrict,
+    required this.locationOtherDetails,
+    required this.businessType,
+    required this.createdBy,
     required this.suspended,
+    required this.suspensionReason,
+    required this.suspensionDuration,
+    this.suspensionStart,
+    this.suspensionEnd,
   });
-
-  Company.empty()
-      : idApi = '',
-        name = '',
-        phoneNumber = '',
-        email = '',
-        companyName = '',
-        registrationDate = DateTime.now(),
-        location = '',
-        createBy = '',
-        suspended = false;
 
   factory Company.fromJson(Map<String, dynamic> json) {
     return Company(
-      idApi: json['_id'] ?? "",
-      name: json['properties']['name'] ?? "",
-      phoneNumber: json['properties']['phoneNumber'] ?? "",
-      email: json['properties']['email'] ?? "",
-      companyName: json['companyName'] ?? "",
+      companyName: json['companyName'],
       registrationDate: DateTime.parse(json['registrationDate']),
-      location: json['location'] ?? "",
-      createBy: json['createBy'] ?? "",
-      suspended: json['suspended'] ?? false,
+      ceoName: json['ceo']['name'],
+      ceoPhoneNumber: json['ceo']['phoneNumber'],
+      ceoEmail: json['ceo']['email'],
+      locationCountry: json['location']['country'],
+      locationCity: json['location']['city'],
+      locationDistrict: json['location']['district'],
+      locationOtherDetails: json['location']['otherDetails'],
+      businessType: json['businessType']['type'],
+      createdBy: json['createdBy'],
+      suspended: json['suspension']['suspended'],
+      suspensionReason: json['suspension']['reason'],
+      suspensionDuration: json['suspension']['duration'],
+      suspensionStart: json['suspension']['suspensionStart'] != null
+          ? DateTime.parse(json['suspension']['suspensionStart'])
+          : null,
+      suspensionEnd: json['suspension']['suspensionEnd'] != null
+          ? DateTime.parse(json['suspension']['suspensionEnd'])
+          : null,
     );
   }
 }
